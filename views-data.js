bbc7a72602153ee148396ce307955a38ad7e0410
Miguel Peixe <miguel@cardume.art.br>
2014-07-31 00:43:58 -0300

    fix images url

--- views-data.js
+++ views-data.js
@@ -4,19 +4,19 @@ module.exports = {
 	selectedProjects: [
 		{
 			title: 'Rio dos Jogos',
-			img: 'http://api.mapascoletivos.com.br/uploads/images/376_1_1371165797.jpg',
+			img: 'http://api.mapascoletivos.com.br/uploads/images-old/376_1_1371165797.jpg',
 			url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d30103a/',
 			description: 'Jornalismo Colaborativo: cuide do Rio!'
 		},
 		{
 			title: 'São Paulo sem frescura',
-			img: 'http://api.mapascoletivos.com.br/uploads/images/139_1_1328364227.jpg',
+			img: 'http://api.mapascoletivos.com.br/uploads/images-old/139_1_1328364227.jpg',
 			url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d300f60/',
 			description: 'Casas, padarias, botecos, barbearias, lojinhas, qualquer tipo de estabelecimento com um charme de antigamente, que mantiveram suas peculiaridades, sem entrar na onda da "modernização".'
 		},
 		{
 			title: 'Trianon Acessível',
-			img: 'http://api.mapascoletivos.com.br/uploads/images/396_1_1384554991.jpg',
+			img: 'http://api.mapascoletivos.com.br/uploads/images-old/396_1_1384554991.jpg',
 			url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d30104e/',
 			description: 'Observar a situação das calçadas e também em estabelecimentos comerciais no entorno, o excesso de postes nas esquinas, rampas de acesso e vagas para deficientes.'
 		},
@@ -34,7 +34,7 @@ module.exports = {
 		},
 		{
 			title: 'Centros de adoção de animais',
-			img: 'http://api.mapascoletivos.com.br/uploads/images/300_1_1350313222.jpg',
+			img: 'http://api.mapascoletivos.com.br/uploads/images-old/300_1_1350313222.jpg',
 			url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d300ff4/',
 			description: 'Onde adotar um bichinho de estimação (não se esqueça, não compre, adote, tem muitos cães e gatos esperando um dono que os ame)'
 		}
